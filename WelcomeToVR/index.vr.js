a844211a228bbda010004310f14e38f45250f39f
Jay W Johnson <Ripley6811@users.noreply.github.com>
2017-11-07 22:01:32 -0500

    animated bounce example added

--- WelcomeToVR/index.vr.js
+++ WelcomeToVR/index.vr.js
@@ -5,7 +5,44 @@ import {
   Pano,
   Text,
   View,
+    Animated,
 } from 'react-vr';
+
+class Playground extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bounceValue: new Animated.Value(0),
+        };
+    }
+    render() {
+        return (
+            <Animated.Image                         // Base: Image, Text, View
+                source={{uri: 'http://i.imgur.com/XMKOH81.jpg'}}
+                onEnter={() => this.setState({textColor: 'red'})}
+                onExit={() => this.setState({textColor: 'white'})}
+                style={{
+                    flex: 1,
+                    width: 1,
+                    height: 1,
+                    transform: [                        // `transform` is an ordered array
+                        {scale: this.state.bounceValue},{rotateY: 20},{translate: [-1, 2, -2.4]},  // Map `bounceValue` to `scale`
+                    ]
+                }}
+            />
+        );
+    }
+    componentDidMount() {
+        this.state.bounceValue.setValue(1.1);     // Start large
+        Animated.spring(                          // Base: spring, decay, timing
+            this.state.bounceValue,                 // Animate `bounceValue`
+            {
+                toValue: 1,                         // Animate to smaller size
+                friction: 0.0,                          // Bouncier spring
+            }
+        ).start();                                // Start the animation
+    }
+}
 
 export default class WelcomeToVR extends React.Component {
     constructor() {
@@ -31,7 +68,7 @@ export default class WelcomeToVR extends React.Component {
           }}>
           hello you
         </Text>
-
+        <Playground />
           <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'blue', transform: [{rotateY: -10},{translate: [-1, 1, -2]}]}}>
               <Text style={{fontSize: 0.2, textAlign: 'center'}}>Blue</Text>
           </View>
